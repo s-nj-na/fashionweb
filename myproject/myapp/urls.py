da3812083ffe70ea6a355263d9f0fcfa015a0021
s-nj-na <sanjanalachu2006@gmail.com>
2026-05-31 22:27:14 -0700

    fixed:stage2

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
+from .views import TaskListView,TaskDetailView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", TaskListView.as_view(), name="hello"),
+    path('hello/', TaskListView.as_view(), name='base'),
+
+    
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    #login and logout urls
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #signup url
+    path('signup/', views.signup, name='signup'),
 ]
