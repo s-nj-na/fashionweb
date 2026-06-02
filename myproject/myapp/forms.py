69fbbbbff5edb222e697b0b65b8322b3081d6ae2
s-nj-na <sanjanalachu2006@gmail.com>
2026-06-01 18:14:47 -0700

    updated

---
+++ myproject/myapp/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from.models import Fashion
+
+
+class FashionForm(forms.ModelForm):
+
+    class Meta:
+        model = Fashion
+        fields = ['title', 'description']
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,10 +1,53 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render , redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Fashion
+from .forms import FashionForm
 
-def home(request):
-    fashion_items = Fashion.objects.all()
-    return render(request, 'index.html', {'fashion_items': fashion_items})
+from django.views.generic import ListView,DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Create your views here.
+class TaskListView(LoginRequiredMixin,ListView):
+    model=Fashion
+    template_name='hello'
+    context_object_name='fashions_list'
+
+    def get_context_data(self, **kwargs):
+         context = super().get_context_data(**kwargs)
+         context['task_form']=FashionForm()
+         return context
+    
+    def post(self,request,*args,**kwargs):
+      form=FashionForm(request.POST)
+      if form.is_valid():
+            form.save()
+            return redirect('hello')
+      
+      return self.get(request,*args,**kwargs)
+
+class TaskDetailView(LoginRequiredMixin,DetailView):
+    model=Fashion 
+    template_name='fashion_details.html'   
+    context_object_name='fashion'
 
 
 
+
+
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('hello')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+    
 # Create your views here.
